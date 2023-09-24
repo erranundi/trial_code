34d69a3a737548f496819a3857704f3a89a4e184
Erranundi <otherworldlyfool@gmail.com>
2023-09-24 23:45:08 +0200

    Initial commit
    // Include folder is full of default VEX headers, as it is // my work around for VS Code users
    Nothing to see here just the initial phase of the code

---
+++ include/robot-config.h
@@ -0,0 +1,17 @@
+#include "v5_apitypes.h"
+#include "vex_brain.h"
+#include "vex_drivetrain.h"
+
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Speedbot (4-motor Drivetrain, No Gyro)                    */
+/*                                                                            */
+/*    Name:                                                                   */
+/*    Date:                                                                   */
+/*    Class:                                                                  */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 10, 11, 20   
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "../include/vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+}
